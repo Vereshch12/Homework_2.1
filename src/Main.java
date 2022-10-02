7681f43ab571f3e3fc977169efd8268e78fcc91a
Vereshchagin_Mikhail <farasers@mail.ru>
2022-10-02 20:16:37 +0300

    Задание №6 и 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
 public class Main {
-    public static void changeValue (int value){ // к заданию 5
+    public static void changeValueInt (int value){ // к заданию 5
         value = 22;
+    }
+
+    public static void changeValueInteger (Integer value){ // к заданию 6
+        value = 22;
+    }
+
+    public static void changeValueIntegerArray (Integer [] value){// к заданию 7
+        value = new Integer[] {3, 4};
+    }
+
+    public static void arrayOutput (Integer [] array){// вывод массива
+        System.out.print("valueIntegerArray = ");
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         //задание №5
         int value = 33;
         System.out.println("value = " + value);
-        changeValue(value);
+        changeValueInt(value);
         System.out.println("value = " + value);
 
+        System.out.println();
+
+        //задание №6
+        Integer valueInteger = 33;
+        System.out.println("valueInteger = " + valueInteger);
+        changeValueInteger(valueInteger);
+        System.out.println("valueInteger = " + valueInteger);
+
+        System.out.println();
+
+        //Задание №7
+        Integer[] valueIntegerArray = new Integer[] {1,2};
+        arrayOutput(valueIntegerArray);
+        changeValueIntegerArray(valueIntegerArray);
+        arrayOutput(valueIntegerArray);
+
+        //Задание №8
 
     }
 }
