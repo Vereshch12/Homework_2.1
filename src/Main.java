235e1857e009e61e939b3315d12ee54588785ca9
Vereshchagin_Mikhail <farasers@mail.ru>
2022-10-02 15:28:00 +0300

    Задание №5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static void changeValue (int value){ // к заданию 5
+        value = 22;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание №5
+        int value = 33;
+        System.out.println("value = " + value);
+        changeValue(value);
+        System.out.println("value = " + value);
+
+
     }
 }
